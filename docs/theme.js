005920efa2608ca2dfc7ff4ac366c1fcb70f5dec
Jasper Zhou <71292728+CheesyPudding@users.noreply.github.com>
2026-04-18 05:21:38 -0700

    adjusted route

--- docs/theme.js
+++ docs/theme.js
@@ -68,7 +68,7 @@
         kofiIcon.className = 'support-us-icon';
         kofiIcon.alt = '';
         kofiIcon.decoding = 'async';
-        kofiIcon.src = new URL('./assets/Icons/kofi.png', scriptUrl).href;
+        kofiIcon.src = new URL('../assets/Icons/kofi.png', scriptUrl).href;
         supportLink.appendChild(kofiIcon);
 
         const supportText = document.createElement('span');
